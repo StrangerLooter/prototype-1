fefca5ca2db4b134be5f733cd2b6b358fb684647
StrangerLooter <rv324102@gmail.com>
2025-06-03 20:22:51 +0530

    layout created

--- website/script.js
+++ website/script.js
@@ -1,10 +1,3 @@
-function changeBackground() {
-  const bgColors = ["#FFDDC1", "#C1FFD7", "#C1E1FF", "#F1C1FF"];
-  const btnColors = ["#FF5733", "#33B5E5", "#4CAF50", "#FFC107"];
-
-  const randomIndex = Math.floor(Math.random() * bgColors.length);
-
-  document.body.style.backgroundColor = bgColors[randomIndex];
-  const button = document.querySelector("button");
-  button.style.backgroundColor = btnColors[randomIndex];
+function addToCart(productName) {
+  alert(productName + " added to cart!");
 }
