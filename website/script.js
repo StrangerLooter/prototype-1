512e92b539f12941a84817e54c15c63f6ecab9c4
StrangerLooter <rv324102@gmail.com>
2025-06-03 19:58:43 +0530

    changes color with interation

--- website/script.js
+++ website/script.js
@@ -1,5 +1,10 @@
 function changeBackground() {
-  const colors = ["#FFDDC1", "#C1FFD7", "#C1E1FF", "#F1C1FF"];
-  const randomColor = colors[Math.floor(Math.random() * colors.length)];
-  document.body.style.backgroundColor = randomColor;
+  const bgColors = ["#FFDDC1", "#C1FFD7", "#C1E1FF", "#F1C1FF"];
+  const btnColors = ["#FF5733", "#33B5E5", "#4CAF50", "#FFC107"];
+
+  const randomIndex = Math.floor(Math.random() * bgColors.length);
+
+  document.body.style.backgroundColor = bgColors[randomIndex];
+  const button = document.querySelector("button");
+  button.style.backgroundColor = btnColors[randomIndex];
 }
